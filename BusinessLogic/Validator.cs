d91215000307c7a524e780b4ffba640e9be33e49
Makayla Reese <makaylareese15@gmail.com>
2020-05-26 15:43:42 -0700

    Add project files.

---
+++ BusinessLogic/Validator.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace BusinessLogic
+{
+    public static class Validator
+    {
+        /// <summary>
+        /// Determines if string is a valid Social Security number
+        /// </summary>
+        /// <param name="ssn"></param>
+        /// <returns></returns>
+        public static bool IsSsn(string ssn)
+        {
+            if(ssn == string.Empty)
+            {
+                return false;
+            }
+            Regex pattern = new Regex(@"^\d{ 3 } -?\d{ 2}-?\d{ 4$");
+            return pattern.IsMatch(ssn);
+            
+        }
+
+        /// <summary>
+        /// Checks if a number is within range
+        /// </summary>
+        /// <param name="numToTest">The number to test</param>
+        /// <param name="minValue">The minimum INCLUSIVE range</param>
+        /// <param name="maxValue">The maximum INCLUSIVE range</param>
+        /// <returns></returns>
+        public static bool IsWithinRange(int numToTest, int minValue, int maxValue)
+        {
+            if(numToTest > minValue && numToTest < maxValue)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
